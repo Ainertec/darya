d2d76e9c5aa65e951ee4d613d5513791e6d48f19
cleitonbalonekr <cleitonbaloneker@gmail.com>
2020-10-08 10:24:30 -0300

    Feat: starte user update profile

--- web/src/mcdonutstere/routes/mcdonuts.routes.js
+++ web/src/mcdonutstere/routes/mcdonuts.routes.js
@@ -6,6 +6,7 @@ import Pedido from "../pages/pedido/index";
 import HomeMcDonuts from "../pages/home/index";
 import LoginMcDonuts from "../pages/login/index";
 import CadastroMcDonuts from "../pages/cliente/index";
+import Perfil from "../pages/cliente/userUpdate";
 
 function PrivateRoute({ children, ...rest }) {
   const { signed } = useAuth();
@@ -39,6 +40,9 @@ function rotas() {
       </PrivateRoute>
       <PrivateRoute path="/mcdonuts/pedido">
         <Pedido />
+      </PrivateRoute>
+      <PrivateRoute path="/mcdonuts/perfil">
+        <Perfil />
       </PrivateRoute>
       <Route path="/mcdonuts/login" component={LoginMcDonuts} />
       <Route path="/mcdonuts/cadastrar" component={CadastroMcDonuts} />
